bddf9980e37f995453cfca7b53182936518810b8
devdevdev09 <devdevdevdevdev09@gmail.com>
2020-11-02 22:21:48 +0900

    remove dead code

--- src/main/java/heo/dae/byevirus2/controller/MainController.java
+++ src/main/java/heo/dae/byevirus2/controller/MainController.java
@@ -1,13 +1,10 @@
 package heo.dae.byevirus2.controller;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@Component
 public class MainController {
     @Value("${service.key}")
     public String servicekey;
