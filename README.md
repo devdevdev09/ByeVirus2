e048791a13d3694bb0bb7aebbeb0e48aceef0778
devdevdev09 <devdevdevdevdev09@gmail.com>
2020-11-03 22:01:34 +0900

    call api test

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ OpenApi
 
 ### 2020-11-01
 - open api 신청
+
+### 2020-11-03
+- api 호출안됨("resultMsg":"SERVICE KEY IS NOT REGISTERED ERROR.")
+- 키는 등록했는데??
--- src/main/java/heo/dae/byevirus2/controller/MainController.java
+++ src/main/java/heo/dae/byevirus2/controller/MainController.java
@@ -1,16 +1,44 @@
 package heo.dae.byevirus2.controller;
 
+import java.net.http.HttpResponse;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import jdk.xml.internal.JdkXmlFeatures.XmlFeature;
 
 @RestController
 public class MainController {
     @Value("${service.key}")
-    public String servicekey;
+    public String SERVICE_KEY;
+
+    @Value("${api.endpoint}")
+    String END_POINT;
+
+    @Value("${api.name}")
+    String API_NAME;
 
     @RequestMapping("/test")
     public void test(){
-        System.out.println("serviceKey : " + servicekey);
+        System.out.println("serviceKey : " + SERVICE_KEY);
+
+        String url = END_POINT + API_NAME + "?serviceKey=" + SERVICE_KEY + "&pageNo=1&numOfRows=10&startCreateDt=20200310&endCreateDt=20200315";
+
+        RestTemplate restTemplate = new RestTemplate();
+        Map<String, Object> map = new HashMap<String,Object>();
+
+        HttpEntity entity = new HttpEntity<Map<String,Object>>(map);
+
+        ResponseEntity response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
+
+        System.out.println(response);
+
     }
 }
