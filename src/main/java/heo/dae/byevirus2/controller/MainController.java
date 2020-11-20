8247b7e38cb66a8a6275f1daa952149900f5cfa0
devdevdev09 <devdevdevdevdev09@gmail.com>
2020-11-21 02:19:33 +0900

    idontknow

--- src/main/java/heo/dae/byevirus2/controller/MainController.java
+++ src/main/java/heo/dae/byevirus2/controller/MainController.java
@@ -1,26 +1,17 @@
 package heo.dae.byevirus2.controller;
 
-import java.net.URI;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import heo.dae.byevirus2.service.ApiService;
-import heo.dae.byevirus2.service.SlackService;
 import heo.dae.byevirus2.service.XmlService;
 import heo.dae.byevirus2.utils.RestUtil;
 import heo.dae.byevirus2.vo.Response;
@@ -30,7 +21,6 @@ import heo.dae.byevirus2.vo.Response;
 public class MainController {
     private ApiService apiService;
     private XmlService xmlService;
-    private SlackService slackService;
 
     @Autowired
     RestUtil restUtil;
@@ -38,10 +28,9 @@ public class MainController {
     @Value("${slack.value.hooks}")
     String SLACK_URL;
 
-    public MainController(ApiService apiService, XmlService xmlService, SlackService slackService) {
+    public MainController(ApiService apiService, XmlService xmlService) {
         this.apiService = apiService;
         this.xmlService = xmlService;
-        this.slackService = slackService;
     }
 
     @GetMapping("/virus")
@@ -66,26 +55,15 @@ public class MainController {
 
         String url = apiService.getApiUrl(targetStartDate, targetEndDate, pageNo, numOfRows);
 
-                // Response responseXml = xmlService.parser(response.getBody());
-                Response responseXml = null;
-                // System.out.println(responseXml.body.toString());
-                // System.out.println(responseXml.body.numOfRows);
-        
-                try{
-                    slackService.sendMsg(responseXml, SLACK_URL);
-                }catch(Exception e){
-        
-                }
-
         ResponseEntity<String> response = null;
+        Response responseXml = null;
         try {
-            // response = restUtil.get(url);
+            response = restUtil.get(url);
+            responseXml = xmlService.parser(response.getBody());
         } catch (Exception e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-        
-
         
 
         return responseXml;
